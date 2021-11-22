1ebb49368be4f05359171ab2d36e7ad6ea61b0d5
Margarita <Margarita@DATA_SERVER>
2021-11-22 14:56:33 +0400

    Add project files.

---
+++ ExampleGeneric/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+//ITVDN HomeWork C# Essential Lesson 10
+namespace Generic
+{
+    public interface IMyList<T>
+    {
+        void Add(T element);
+
+        T this[int index] { get; }
+
+        int Count { get; }
+
+        void Clear();
+
+        bool Contains(T item);
+    }
+
+    public class MyList<T> : IMyList<T>
+    {
+        private T[] array;
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T element)
+        {
+            T[] tempArray = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[array.Length] = element;
+            array = tempArray;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public void Clear()
+        {
+            array = new T[0];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array.GetType().GetElementType() == typeof(int))
+                {
+                    if ((int)(object)array[i] == (int)(object)item)
+                    {
+                        return true;
+                    }
+                }
+                else if (array.GetType().GetElementType() == typeof(String))
+                {
+                    if ((string)(object)array[i] == (string)(object)item)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    throw new Exception("TypeMismatchException");
+                }
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an int array length: ");
+            string str = Console.ReadLine();
+            int length = default(int);
+            if (!string.IsNullOrEmpty(str))
+            {
+                length = int.Parse(str);
+            }
+
+            var list = new MyList<int>();
+            var rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(rnd.Next(100));
+            }
+
+            Console.WriteLine("Elements are:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 20));
+
+            Console.Write("Input item for search: ");
+            if (list.Contains(int.Parse(Console.ReadLine())))
+            {
+                Console.WriteLine("The searched element is present in the list");
+            }
+            else
+            {
+                Console.WriteLine("The searched element is not present in the list");
+            }
+
+            Console.Write("Input a string array length: ");
+            str = Console.ReadLine();
+            length = default(int);
+            if (!string.IsNullOrEmpty(str))
+            {
+                length = int.Parse(str);
+            }
+
+            var listStr = new MyList<string>();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Input string: ");
+                listStr.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("Elements are:");
+            for (int i = 0; i < listStr.Count; i++)
+            {
+                Console.Write($"{listStr[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 20));
+
+            Console.Write("Input item for search: ");
+            if (listStr.Contains(Console.ReadLine()))
+            {
+                Console.WriteLine("The searched element is present in the list");
+            }
+            else
+            {
+                Console.WriteLine("The searched element is not present in the list");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _Indexers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _indexers
+{
+    class Dictionary
+    {
+        private string[] value = new string[7];
+        private int[] keyInt = new int[7];
+        private string[] keyString = new string[7];
+
+        public Dictionary()
+        {
+            value[0] = "Monday";    keyString[0] = "Երկուշաբթի";    keyInt[0] = 1;
+            value[1] = "Tuesday";   keyString[1] = "Երեքշաբթի";     keyInt[1] = 2;
+            value[2] = "Wednesday"; keyString[2] = "Չորեքշաբթի";    keyInt[2] = 3;
+            value[3] = "Thursday";  keyString[3] = "Հինգշաբթի";     keyInt[3] = 4;
+            value[4] = "Friday";    keyString[4] = "Ուրբաթ";        keyInt[4] = 5;
+            value[5] = "Saturday";  keyString[5] = "Շաբաթ";         keyInt[5] = 6;
+            value[6] = "Sunday";    keyString[6] = "Կիրակի";        keyInt[6] = 7;
+        }
+
+        public string this[string index]
+        {
+            get 
+            {
+                for (int i = 0; i < keyString.Length; i++)
+                    if (keyString[i] == index)
+                        return  value[i] + new string('-', 10) + keyString[i];
+                return null;
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                for (int i = 0; i < keyInt.Length; i++)
+                    if (keyInt[i] == index)
+                        return keyInt[i] + "\t" + value[i];
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(dictionary["Երկուշաբթի"]);
+            Console.WriteLine(dictionary["Երեքշաբթի"]);
+            Console.WriteLine(dictionary["Չորեքշաբթի"]);
+            Console.WriteLine(dictionary["Հինգշաբթի"]);
+            Console.WriteLine(dictionary["Ուրբաթ"]);
+            Console.WriteLine(dictionary["Շաբաթ"]);
+            Console.WriteLine(dictionary["Կիրակի"]);
+            
+            for (int i = 1; i < dictionary.keyInt.Length + 1; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
